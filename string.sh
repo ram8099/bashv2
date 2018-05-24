b1f7640919b6e60dd7531e8c65323b6090b1743b
rammmmmm <ramakrishna.s.s@ncodeit.com>
2018-05-24 11:45:43 +0530

    string.sh

---
+++ string.sh
@@ -0,0 +1,15 @@
+usage() {
+  cat <<-EOF
+  Usage: pgihadmin tache <CIBLE> [-f] [-q] [-h]
+  Cette tache permet de bla bla
+  PARAMETRES:
+  ===========
+      CIBLE    Serveur cible : <vide> ou all, aps, ts, lb, ord, apsN, tsN, lbN, ordN (avec N un nombre)
+  OPTIONS:
+  ========
+      -f    Mode force
+      -q    Mode silencieux
+      -h    Affiche ce message
+  EOF
+}
+
